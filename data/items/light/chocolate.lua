30a01f3bffef27c9a7ff14637720186e67e7af42
HUECYCLES <luhlinard@gmail.com>
2025-03-10 17:14:33 -0300

    chocolate item

--- data/items/light/chocolate.lua
+++ data/items/light/chocolate.lua
@@ -1,7 +1,7 @@
 local item, super = Class(Item, "light/chocolate")
 
 function item:init()
-    super:init(self)
+    super.init(self)
 
     self.name = "Chocolate"
     
@@ -15,25 +15,23 @@ function item:init()
 
     self.usable_in = "all"
 
-    self.buy_price = 0
+    self.buy_price = 2
     self.sell_price = 0
 end
 
 function item:onWorldUse()
     Game.world:startCutscene(function(cutscene)
-        cutscene:text("* You took the chocolate from \nyour pocket.[wait:5]")
+        cutscene:text("* You took the chocolate from \nyour pocket.")
         cutscene:text("* You remember these as one of those cheap chocolates with a soapy aftertaste.")
-        cutscene:text("* ...[wait:10]\n* Your dad used to buy them frequently.")
+        cutscene:text("* ...[wait:5]\n* Your dad used to buy them frequently.")
         cutscene:text("* Eat it anyway?")
 
-        local dropped
-        dropped = cutscene:choicer({"Yes", "No"}) == 1 --setting as "2" inverts "Yes" and "No" like it is in Chapter 1.
+        local eat = cutscene:choicer({"Yes", "No"}) == 1
 
-        if dropped then
+        if eat then
             Game.inventory:removeItem(self)
-            Assets.playSound("item")
+            Assets.playSound("swallow")
             cutscene:text("* You felt nostalgic and ate the entire bar in one go.")
-
         else
             cutscene:text("* You looked at the chocolate bar with disgust.")
         end
@@ -41,10 +39,10 @@ function item:onWorldUse()
     return false
 end
 
-function item:convertToDark(inventory)
+--[[function item:convertToDark(inventory)
     local chocolate = inventory:addItem("chocolate")
     chocolate.flags = self.flags
     return true
-end
+end]]
 
 return item
