d610ddd58918e34c4be464d092c3e14122077404
HUECYCLES <luhlinard@gmail.com>
2025-03-10 17:13:53 -0300

    apple item

--- data/items/light/apple.lua
+++ data/items/light/apple.lua
@@ -20,22 +20,35 @@ function item:init()
 end
 
 function item:onWorldUse()
-    if self:getFlag("interact_count") == 0 then
-        Assets.playSound("item")
-        Game.world:showText("* You nibbled the apple.")
-        self:addFlag("interact_count")
-        return false
-    elseif self:getFlag("interact_count") == 1 then
-        Assets.playSound("item")
-        Game.world:showText("* You nibbled the apple.")
-        self:addFlag("interact_count")
-        return false
-    elseif self:getFlag("interact_count") == 2 then
-        Assets.playSound("item")
-        Game.world:showText("* Congratulations! You ate the entire apple.")
-        self:setFlag("interact_count", 0)
-        return true
-    end
+    Game.world:startCutscene(function(cutscene)
+        if self:getFlag("interact_count") == 0 then
+            Assets.playSound("swallow")
+            cutscene:text("* You nibbled the apple.")
+            self:addFlag("interact_count")
+            return false
+        elseif self:getFlag("interact_count") == 1 then
+            Assets.playSound("swallow")
+            cutscene:text("* You nibbled the apple again.")
+            self:addFlag("interact_count")
+            return false
+        elseif self:getFlag("interact_count") == 2 then
+            Assets.playSound("swallow")
+            cutscene:text("* You nibbled absent-mindedly until you got to the core.")
+            if Game.world:getCharacter("susie") and #Game.party > 1 then
+                cutscene:setSpeaker("susie")
+                cutscene:text("* You, [wait:5]uh, [wait:5]gonna eat that?", "nervous")
+                cutscene:text("* Cuz. [wait:5]Y'know, [wait:5]if you're just gonna trash it...", "nervous_side")
+                cutscene:text("* Nice! [wait:5]Thanks!", "surprise_smile")
+                cutscene:setSpeaker()
+                cutscene:text("* You threw the apple at Susie. [wait:5]She chomped down on it instantly.")
+            else
+                cutscene:text("* You tossed it.")
+            end
+            Game.inventory:removeItem(self)
+            self:setFlag("interact_count", 0)
+            return true
+        end
+    end)
 end
 
 return item
