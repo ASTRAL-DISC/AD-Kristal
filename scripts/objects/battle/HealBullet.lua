e0df79e7d395b10ad5a4060b90f26ba04db77a83
HUECYCLES <luhlinard@gmail.com>
2025-03-20 20:09:34 -0300

    a few changes to bullets

--- scripts/objects/battle/HealBullet.lua
+++ scripts/objects/battle/HealBullet.lua
@@ -1,20 +1,20 @@
 -- original library by KateBulka
-local GreenBullet, super = Class(Bullet)
+local HealBullet, super = Class(Bullet)
 
-function GreenBullet:init(x, y, texture)
+function HealBullet:init(x, y, texture)
 	super.init(self, x, y, texture)
-	self:setColor(71 / 255, 1, 0)
+	self:setColor(0, 1, 0)
     self.layer = BATTLE_LAYERS["soul"] - 1
 	self.tp = 0
 	self.damage = 0
 	self.heal = 15
 end
 
-function GreenBullet:onCollide(soul)
+function HealBullet:onCollide(soul)
 	local member = Utils.pick(Game.battle.party)
 	member:heal(self.heal)
 	
 	self:remove()
 end
 
-return GreenBullet
+return HealBullet
