8d96fee21aa2f906629199542e4ec12929a3a01b
HUECYCLES <luhlinard@gmail.com>
2025-03-21 22:19:07 -0300

    removed toriel weird route scene

--- scripts/world/cutscenes/hometown/dreemurr.lua
+++ scripts/world/cutscenes/hometown/dreemurr.lua
@@ -25,89 +25,35 @@ return {
         cutscene:text("* (...and turned it into a heavy, [wait:5]wagon-ball.)")
         Game.lock_movement = false
     end,
+
     bathroom = function(cutscene, event)
         cutscene:text("* (No need for it right now.)")
     end,
-    torieldoor = function(cutscene, event)
-        cutscene:text("* (You hear your mother\nwhispering to herself...)")
-        if Game:getFlag("stormeyed") then
-        end
-        cutscene:wait(1)
-        local black = Rectangle(0, 0, Game.world.width, Game.world.height)
-        black:setColor(0, 0, 0)
-        black.alpha = 0
-        Game.world:spawnObject(black, "below_ui")
-        Game.world.timer:tween(2, black, {alpha = 1})
-        cutscene:wait(2)
-        Game.world.music:fade(0, 1)
-        cutscene:wait(1)
-        Game.world.music:stop()
-        cutscene:wait(1)
-        Game.world.music:play("denial", 0.8, 0.94)
-        cutscene:text("* [speed:0.5](... [wait:5]keep these logs as a way to...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... \"Find\" myself among all this.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](And to register what has caught my eye...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](I am... [wait:5]so worried about\nKris, [wait:5]lately.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](There is something they are\nnot telling me.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](Something that seems to make them uncomfortable.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... [wait:5]I always tell them:)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](\"Kris...\")\n[wait:5]* (\"If you need anything,\n[wait:5]simply call.\")", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](\"Kris, [wait:5]will you promise...\")", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](\"... to tell me what might be going on, [wait:5]if you ever find yourself in a...\")", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](\"'scary' situation?\")", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](Why is it that whenever I try to get closer to my child...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](They seem to slip farther away...?)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](They refuse to talk to me.)\n[wait:5]* (I always have to go to them first.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](And lately, [wait:5]it seems like...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... Ah, [wait:5]it has just gotten worse... [wait:5]has it not?)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](What am I supposed to do as a mother if I cannot even have a conversation with my child?)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... [wait:10]Did I fail?)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](Not to mention...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](All of this is getting to me\nin the worst of ways...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](I am not one to keep a dream diary, [wait:5]but...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](Yesterday marked the strangest experience I have ever had.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](A lucid dream... [wait:5]One so real that I cannot... [wait:5]bring myself\nto believe it.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](I cannot believe in what I saw.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... [wait:5]That was not my child.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](They would never...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](Suddenly, [wait:5]the thought of them having a knife of their own...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](Sounds...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... [wait:5]No, [wait:5]no, [wait:5]Tori. [wait:5]Please do\nnot do this.)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](What can a mother do...)", nil, "toriel_lw")
-        Game.world.music:fade(0, 1)
-        cutscene:wait(1)
-        Game.world.music:stop()
-        cutscene:text("* [speed:0.5](If she finds herself afraid of her own child?)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](...)", nil, "toriel_lw")
-        cutscene:text("* [speed:0.5](... [wait:5]I need to rest.)", nil, "toriel_lw")
-        cutscene:wait(2)
-        black:fadeOutAndRemove(2)
-        cutscene:text("* (You should leave.)")
-        Game.world.music:play("crickets", 0)
-        Game.world.music:fade(1, 1)
-    end,
+
     wrongway = function(cutscene, event)
         cutscene:text("* (Can't let your mother see you carrying things out of your house.)")
     end,
+
     wrongway2 = function(cutscene, event)
         cutscene:text("* (Not the way to the school.)")
     end,
+
     computer = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     drawer1 = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     drawer2 = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     window = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     krisbed = function(cutscene, event)
         local kris = cutscene:getCharacter("kris")
         local bedsheet = cutscene:getEvent("bedsheet")
@@ -150,27 +96,34 @@ return {
             Game.lock_movement = false
             cutscene:attachCamera()
 		else
-			cutscene:text("* (That's fine.)")
+			cutscene:text("* (You will sleep later.)")
 		end
     end,
+
     birdcage = function(cutscene, event)
-        cutscene:text("* Placeholder!")
+        cutscene:text("* It's a birdcage. [wait:5]When the door's closed, [wait:5]there's no escape.")
     end,
+
     stain = function(cutscene, event)
-        cutscene:text("* Placeholder!")
+        cutscene:text("* It's stained.")
     end,
+
     mirror = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     shelf = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     cactus = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     books = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
+
     phone = function(cutscene, event)
         cutscene:text("* Placeholder!")
     end,
