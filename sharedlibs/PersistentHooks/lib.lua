28d04785d23ebd5072e3d48ddb3682c17f0fc036
HUECYCLES <luhlinard@gmail.com>
2025-03-27 18:58:03 -0300

    PersistentHooks updates

--- sharedlibs/PersistentHooks/lib.lua
+++ sharedlibs/PersistentHooks/lib.lua
@@ -1,55 +1,8 @@
 local Lib = {}
 
 function Lib:init()
-    self:loadDebugHooks()
     self:loadEncounterHooks()
     self:loadBattleHooks()
-end
-
-function Lib:loadDebugHooks()
-    -- DebugSystem Custom Menus
-    Utils.hook(DebugSystem, "registerSubMenus", function(orig, self)
-        orig(self)
-
-        self:registerOption("main", "Heal Party", "Heal the whole party to max health.", function ()
-            if Game.battle then
-                for _,party in ipairs(Game.battle.party) do
-                    party:heal(math.huge)
-                end
-            else
-                for i, chara in ipairs(Game.party) do
-                    local prev_health = chara:getHealth()
-                    chara:heal(math.huge, false)
-                    local amount = chara:getHealth() - prev_health
-        
-                    if not Game:isLight() and Game.world.healthbar then
-                        local actionbox = Game.world.healthbar.action_boxes[i]
-                        local text = HPText("+" .. amount, Game.world.healthbar.x + actionbox.x + 69, Game.world.healthbar.y + actionbox.y + 15)
-                        text.layer = WORLD_LAYERS["ui"] + 1
-                        Game.world:addChild(text)
-                    end
-                end
-        
-                Assets.stopAndPlaySound("power")
-            end
-        end)
-
-        self:registerOption("main", "Give Money", "Give money to party.", function ()
-            self.window = DebugWindow("Enter Money", "Enter the money amount you'd like.", "input", function (text)
-                local money = tonumber(text)
-                if money then
-                    if Game:isLight() then
-                        Game.lw_money = Game.lw_money + money
-                    else
-                        Game.money = Game.money + money
-                    end
-                    Assets.stopAndPlaySound("cash")
-                end
-            end)
-            self.window:setPosition(Input.getCurrentCursorPosition())
-            self:addChild(self.window)
-        end)
-    end)
 end
 
 function Lib:loadEncounterHooks()
@@ -83,6 +36,26 @@ function Lib:loadBattleHooks()
             end
         end
         return tbl
+    end)
+
+    Utils.hooks(Battle, "onStateChange", function(orig, self, old, new)
+        orig(self, old, new)
+        
+        if self.discoball then
+            if new == "TRANSITION" then
+                self.discoball.y = 0
+            elseif new == "ACTIONSELECT" then
+                self.discoball.tweendir = 1
+            elseif new == "ENEMYDIALOGUE" or new == "DEFENDINGBEGIN" then
+                self.discoball.tweendir = -1
+            elseif new == "VICTORY" or new == "TRANSITIONOUT" then
+                if self.discoball.y == 0 then
+                    self.discoball.y = 0
+                else
+                    self.discoball.tweendir = 1
+                end
+            end
+        end
     end)
 end
 
