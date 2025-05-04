df4cfce635d69666357ce4a7170236f501b78b79
HUECYCLES <luhlinard@gmail.com>
2025-05-04 16:21:10 -0300

    nodiscstate grid update

--- src/nodiscstate.lua
+++ src/nodiscstate.lua
@@ -28,13 +28,13 @@ function NoDisc:draw()
     for i = 2, 22 do
         Draw.setColor(COLORS.green, 0.3)
         love.graphics.line(0, -210 + (i * 50) + math.floor(self.offset / 2), SCREEN_WIDTH, -210 + (i * 50) + math.floor(self.offset / 2))
-        love.graphics.line(-200 + (i * 50) + math.floor(self.offset / 2), 0, -200 + (i * 50) + math.floor(self.offset / 2), SCREEN_HEIGHT)
+        love.graphics.line(-100 + (i * 50) + math.floor(self.offset / 2), 0, -200 + (i * 50) + math.floor(self.offset / 2), SCREEN_HEIGHT)
     end
 
     for i = 3, 23 do
         Draw.setColor(COLORS.green, 0.15)
         love.graphics.line(0, -100 + (i * 50) - math.floor(self.offset), SCREEN_WIDTH, -100 + (i * 50) - math.floor(self.offset))
-        love.graphics.line(-100 + (i * 50) - math.floor(self.offset), 0, -100 + (i * 50) - math.floor(self.offset), SCREEN_HEIGHT)
+        love.graphics.line(-200 + (i * 50) - math.floor(self.offset), 0, -100 + (i * 50) - math.floor(self.offset), SCREEN_HEIGHT)
     end
     
     Draw.setColor(1, 1, 1, 1)
