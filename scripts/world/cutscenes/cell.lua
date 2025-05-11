70dc6e94274db1eca2de9a2b5d67e55f11eec2e9
HUECYCLES <luhlinard@gmail.com>
2025-05-11 20:45:55 -0300

    cell.him test cutscene

--- scripts/world/cutscenes/cell.lua
+++ scripts/world/cutscenes/cell.lua
@@ -37,47 +37,101 @@ return {
         end
     end,
     him = function(cutscene)
+        Game:setDepths(true)
         Game.world.music:pause()
-        cutscene:during(function ()
-            if Game.world.cutscene.textbox.box.visible then
-                Game.world.cutscene.textbox.box.visible = false
-            end
-        end)
 
         local black = Rectangle(0, 0, Game.world.width, Game.world.height)
         black:setColor(COLORS.black)
         black.alpha = 1
         Game.world:spawnObject(black, "below_ui")
 
-        Assets.playSound("phone", 0.7)
-        cutscene:text("* (Ring, [wait:5]ring...)")
+        cutscene:wait(2)
+
+        cutscene:wait(cutscene:playSound("phone", 0.7, 0.8))
+        cutscene:text("* ... [wait:5]?\n[wait:10]* There's the phone.")
+        cutscene:text("* Instinctively, [wait:5]you reach for your pocket.")
+        cutscene:text("* But you did not have a phone with you.")
+
+        cutscene:wait(0.5)
+
+        cutscene:wait(cutscene:playSound("phone", 0.7, 0.8))
+        cutscene:text("* ... [wait:5]?\n[wait:10]* There's the phone.")
+        cutscene:text("* Instinctively, [wait:5]you search your hands.")
+        cutscene:text("* But they were only grasping at nothing.")
+
+        cutscene:wait(0.5)
+
+        cutscene:wait(cutscene:playSound("phone", 0.7, 0.78))
+        cutscene:text("* ... [wait:5]?\n[wait:10]* There's the phone.")
+        cutscene:text("* This time, [wait:5]it echoes inside your heart.")
+        cutscene:text("* It hurts.")
+        cutscene:text("* But through the pain,")
+        cutscene:text("* you take a deep breath,")
+        cutscene:text("* [wait:10]and answer it genuinely.")
         if Game.world:getCellFlag("cell.him", 0) > 0 then
             cutscene:text("* (No one picked up.)")
         else
-            cutscene:wait(1)
+            cutscene:wait(2)
 
             local GSTYLE = "[speed:0.4][spacing:6][style:GONER]"
             cutscene:setSpeaker("gaster")
 
-            cutscene:text(GSTYLE .."[wait:80]TESTING WILL COMMENCE", "apathy")
-            cutscene:text(GSTYLE .."IN THREE\n[wait:40]TWO\n[wait:80]ONE[wait:10].", "apathy")
-            cutscene:text(GSTYLE .."...", "insensitivity")
-            cutscene:text(GSTYLE .."... [wait:40]ARE YOU THERE?", "taciturn")
-            cutscene:text(GSTYLE .."YES, [wait:40]OF COURSE.\n[wait:40]OF COURSE YOU ARE.", "aloof")
-            cutscene:text(GSTYLE .."DESPITE ALL ODDS,\n[wait:40]HOW HAVE YOU BEEN?", "wistful")
-            cutscene:text(GSTYLE .."... PERHAPS\n[wait:40]NOT THE MOST ADEQUATE QUESTION.", "euphoria")
-            cutscene:text(GSTYLE .."APOLOGIES FOR PULLING YOU OUT OF YOUR...", "choleric")
-            cutscene:text(GSTYLE .."COMFORT ZONE.", "apathy")
-            cutscene:text(GSTYLE .."THE FACT YOU CAN CALL IT \"COMFORT\" IS [wait:40]INTRIGUING.", "wistful")
-            cutscene:text(GSTYLE .."PLEASE [wait:40]DO NOT LOOK SO SULLEN.", "insensitivity")
-            cutscene:text(GSTYLE .."I'D LIKE TO SHARE SOMETHING WITH YOU.", "euphoria")
-            cutscene:text(GSTYLE .."BUT...", "aloof")
-            cutscene:text(GSTYLE .."I WORRY.", "worry")
+            cutscene:text(GSTYLE .." ", "apathy")
+            --[[cutscene:text(GSTYLE .."... [wait:40]ARE YOU THERE?", "taciturn")
+            cutscene:text(GSTYLE .."...", "vacillation")
 
-            cutscene:wait(6)
+            cutscene:text(GSTYLE .."YES, [wait:40]OF COURSE.\n[wait:40]OF COURSE YOU ARE.", "aloof")
+            cutscene:text(GSTYLE .."THERE IS A REASON\n[wait:40]I CONTACTED YOU.", "insensitivity")
+            cutscene:text(GSTYLE .."JUST AS THERE IS A REASON [wait:40]YOU ANSWERED.", "insensitivity")]]
+
+            cutscene:text(GSTYLE .."[wait:40][color:green][6[wait:20]/4[wait:20]/2[wait:5]0[wait:5]2[wait:5]5]", "aloof")
+            cutscene:text(GSTYLE .."IN [wait:40][color:green][2[wait:20]3][color:reset] [wait:40]DAYS,", "vacillation")
+            cutscene:text(GSTYLE .."YOU WILL SEE SOMETHING\n[wait:40]QUITE WONDERFUL.", "euphoria")
+            cutscene:text(GSTYLE .."MY HANDS TREMBLE[wait:40]\nWITH EXCITEMENT.", "choleric")
+            cutscene:text(GSTYLE .."PERHAPS THIS FEELING IS [wait:40]SHARED BY MILLIONS?", "contempt")
+
+            cutscene:text(GSTYLE .."...", "taciturn")
+
+            --[[cutscene:text(GSTYLE .."DESPITE ALL ODDS,\n[wait:40]HOW HAVE YOU BEEN?", "wistful")
+            cutscene:text(GSTYLE .."... PERHAPS.\n[wait:40]THIS QUESTION IS [wait:40]UNWARRANTED.", "choleric")
+            cutscene:text(GSTYLE .."HOWEVER, I FEEL\n[wait:40]INCLINED TO ASK IT.", "trepidation")
+            cutscene:text(GSTYLE .."[wait:40]THERE IS [wait:40]AN ODD COMFORT [wait:40]IN UNCHANGING SCENERY.", "wistful")
+            cutscene:text(GSTYLE .."PERHAPS. [wait:40]THE FEELING OF KNOWING THAT PLACE", "euphoria")
+            cutscene:text(GSTYLE .."KNOWING IT WILL\n[wait:40]NEVER BETRAY YOU.", "euphoria")
+            cutscene:text(GSTYLE .."AN ISLAND\n[wait:40]UNTO YOURSELF.", "ire")
+            cutscene:text(GSTYLE .."[wait:80]A STATE OF PURE\n[wait:40]IGNORANT\n[wait:40]BLISS.", "wistful")
+            cutscene:text(GSTYLE .."NO MATTER WHAT IS HAPPENING OUTSIDE THESE WALLS,", "vacillation")
+            cutscene:text(GSTYLE .."OR THE LACK OF THEM.", "aggravation")
+
+            cutscene:text(GSTYLE .."HOWEVER.", "trepidation")
+            cutscene:text(GSTYLE .."YOUR WORLD IS NEVER SO UNCHANGING.", "trepidation")
+            cutscene:text(GSTYLE .."NO MATTER HOW MUCH [wait:40]YOU'VE WANTED IT TO BE.", "ire")
+            cutscene:text(GSTYLE .."ON SLEEPLESS NIGHTS\n[wait:40]ON DORMANT DAYS.", "taciturn")
+            cutscene:text(GSTYLE .."IT IS DIZZYING IN WAYS I CANNOT COMPREHEND.", "worry")
+            cutscene:text(GSTYLE .."MAKE ME UNDERSTAND.", "aggravation")
+            cutscene:text(GSTYLE .."THIS EXHILARATION [wait:40]YOU FEEL", "contempt")
+            cutscene:text(GSTYLE .."IT IS THE SAME\n[wait:40]AS MINE.", "vacillation")
+            cutscene:text(GSTYLE .."BUT WE LOOK THROUGH [wait:40]DIFFERENT LENSES.", "aloof")
+            cutscene:text(GSTYLE .."WHAT YOU HAVE\n[wait:40]IS NOT ENOUGH.", "trepidation")
+            cutscene:text(GSTYLE .."WHAT I HAVE", "aggravation")]]
+
+            --cutscene:text(GSTYLE .."", "taciturn")
+
+            cutscene:text(GSTYLE .."WHEN THAT DAY COMES, [wait:40]REMEMBER", "wistful")
+            cutscene:text(GSTYLE .."YOU DESERVE\n[wait:40]TO REST.", "ire")
+            cutscene:text(GSTYLE .."MISERY, [wait:40]MISFORTUNE. [wait:40]THEY WILL TAKE THEIR LEAVE.", "trepidation")
+            cutscene:text(GSTYLE .."EVERY STEP YOU TAKE\n[wait:40]AT YOUR HEART'S EVERY PULSE", "worry")
+            cutscene:text(GSTYLE .."IF YOU WISH FOR EVEN A HINT OF SAFETY", "aggravation")
+            cutscene:text(GSTYLE .."THEN THAT WISH\n[wait:40]WILL BE MADE VALID.", "wistful")
+            cutscene:text(GSTYLE .."[wait:80]IT IS WAITING.", "euphoria")
+
+            cutscene:wait(3)
 
             cutscene:setSpeaker()
             cutscene:text("* (Click...)")
+
+            cutscene:wait(3)
+            Game:setDepths(false)
             black:fadeOutAndRemove(1)
             Game.world.music:resume()
         end
