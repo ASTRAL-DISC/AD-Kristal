b1c8ff9c0381957f2dd0df027b615ead52de3556
HUECYCLES <luhlinard@gmail.com>
2025-05-26 17:54:47 -0300

    optimize regroup cutscene

--- scripts/world/cutscenes/regroup.lua
+++ scripts/world/cutscenes/regroup.lua
@@ -1,64 +1,69 @@
---code made originally by Nyako!
-return {
-    fungang = function(cutscene)
+-- code made originally by Nyako!
+return{
+    default = function(cutscene)
+        if #Game.party ~= 3 then return end
+
         Assets.playSound("jump")
-        
         cutscene:resetSprites()
-        
-        local kris = cutscene:getCharacter("kris")
-        local susie = cutscene:getCharacter("susie")
-        local ralsei = cutscene:getCharacter("ralsei")
         
         cutscene:detachCamera()
         cutscene:detachFollowers()
 
-        kris.sprite:set("landed_1")
-        susie.sprite:set("landed_1")
-        ralsei.sprite:set("landed_1")
+        for _, member in ipairs(Game.party) do
+            cutscene:getCharacter(member.id).sprite:set("landed_1")
+        end
         cutscene:wait(2/30)
         
-        kris:jumpTo(susie.x + 80, susie.y, 12, 17/30, "fall_1", "landed_1")
-        ralsei:jumpTo(susie.x + 80, susie.y, 12, 17/30, "fall", "landed_1")
-        susie:jumpTo(susie.x + 80, susie.y, 12, 17/30, "fall_1", "landed_1")
+        local p1 = cutscene:getCharacter(Game.party[1].actor.id)
+        local p2 = cutscene:getCharacter(Game.party[2].actor.id)
+        local p3 = cutscene:getCharacter(Game.party[3].actor.id)
+
+        local fall_sprite = p3.actor.id == "ralsei" and "fall" or "fall_1"
+
+        p1:jumpTo(p2.x + 80, p2.y, 12, 17/30, fall_sprite, "landed_1")
+        p2:jumpTo(p2.x, p2.y, 12, 17/30, fall_sprite, "landed_1")
+        p3:jumpTo(p2.x - 80, p2.y, 12, 17/30, fall_sprite, "landed_1")
 
         cutscene:wait(20/30)
         Assets.playSound("impact")
         cutscene:wait(16/30)
         Assets.playSound("swing")
 
-        kris:spin(2)
-        susie:spin(2)
-        ralsei:spin(2)
+        for _, member in ipairs(Game.party) do
+            cutscene:getCharacter(member.id):spin(2)
+        end
 
         cutscene:wait(16/30)
         Assets.playSound("bell")
         
-        kris:spin(0)
-        susie:spin(0)
-        ralsei:spin(0)
-        kris.sprite:set("pose")
-        susie.sprite:set("pose")
-        ralsei.sprite:set("pose")
+        for _, member in ipairs(Game.party) do
+            cutscene:getCharacter(member.id):spin(0)
+            cutscene:getCharacter(member.id).sprite:set("pose")
+        end
+
         cutscene:wait(15/30)
         cutscene:resetSprites()
-        
-        kris:setFacing("right")
-        susie:setFacing("right")
-        ralsei:setFacing("right")
+
+        for _, member in ipairs(Game.party) do
+            cutscene:getCharacter(member.id):setFacing("right")
+        end
+
         cutscene:attachCamera()
         cutscene:interpolateFollowers()
         cutscene:attachFollowers()
     end,
-    knr_1st = function(cutscene)
-        Assets.playSound("jump")
-        
-        cutscene:resetSprites()
-        
+    noelle = function(cutscene)
+        if not cutscene:getCharacter("ralsei") and cutscene:getCharacter("noelle") then return end
+
         local kris = cutscene:getCharacter("kris")
         local ralsei = cutscene:getCharacter("ralsei")
         local noelle = cutscene:getCharacter("noelle")
 
-        cutscene:detachCamera()
+        Assets.playSound("jump")
+        
+        cutscene:resetSprites()
+
+        cutscene:panTo(noelle.x, noelle.y)
         cutscene:detachFollowers()
 
         kris.sprite:set("landed_1")
@@ -126,108 +131,6 @@ return {
         kris:setFacing("right")
         noelle:setFacing("right")
         ralsei:setFacing("right")
-        cutscene:attachCamera()
-        cutscene:interpolateFollowers()
-        cutscene:attachFollowers()
-    end,
-    knr = function(cutscene)
-        Assets.playSound("jump")
-        
-        cutscene:resetSprites()
-        
-        local kris = cutscene:getCharacter("kris")
-        local ralsei = cutscene:getCharacter("ralsei")
-        local noelle = cutscene:getCharacter("noelle")
-        
-        cutscene:detachCamera()
-        cutscene:detachFollowers()
-
-        kris.sprite:set("landed_1")
-        ralsei.sprite:set("landed_1")
-        noelle.sprite:set("landed_1")
-        cutscene:wait(1/30)
-        
-        kris:jumpTo(noelle.x + 80, noelle.y, 12, 17/30, "fall_1", "landed_1")
-        ralsei:jumpTo(noelle.x - 80, noelle.y, 12, 17/30, "fall", "landed_1")
-        noelle:jumpTo(noelle.x, noelle.y, 12, 17/30, "fall_1", "landed_1")
-
-        cutscene:wait(20/30)
-        Assets.playSound("impact")
-        cutscene:wait(16/30)
-        Assets.playSound("swing")
-
-        kris:spin(2)
-        noelle:spin(2)
-        ralsei:spin(2)
-
-        cutscene:wait(16/30)
-        Assets.playSound("bell")
-        
-        kris:spin(0)
-        noelle:spin(0)
-        ralsei:spin(0)
-        kris.sprite:set("pose")
-        noelle.sprite:set("pose")
-        ralsei.sprite:set("pose")
-        cutscene:wait(15/30)
-        cutscene:resetSprites()
-        
-        kris:setFacing("right")
-        noelle:setFacing("right")
-        ralsei:setFacing("right")
-        cutscene:attachCamera()
-        cutscene:interpolateFollowers()
-        cutscene:attachFollowers()
-    end,
-    knb = function(cutscene)
-        Assets.playSound("jump")
-        
-        cutscene:resetSprites()
-        
-        local kris = cutscene:getCharacter("kris")
-        local noelle = cutscene:getCharacter("noelle")
-        local berdly = cutscene:getCharacter("berdly")
-        
-        cutscene:detachCamera()
-        cutscene:detachFollowers()
-
-        kris.sprite:set("landed_1")
-        noelle.sprite:set("landed_1")
-        berdly.sprite:set("landed_2")
-        cutscene:wait(1/30)
-        
-        kris:jumpTo(kris.x, kris.y, 12, 17/30, "fall_1", "landed_1")
-        noelle:jumpTo(kris.x + 80, kris.y, 12, 17/30, "fall", "landed_1")
-        berdly:jumpTo(kris.x - 80, kris.y, 12, 17/30, "jump_2", "landed_2")
-
-        cutscene:wait(20/30)
-        Assets.playSound("impact")
-        cutscene:wait(16/30)
-        Assets.playSound("swing")
-
-        kris:spin(2)
-        noelle:spin(2)
-        berdly:spin(2)
-
-        cutscene:wait(16/30)
-        Assets.playSound("bell")
-        
-        kris:spin(0)
-        noelle:spin(0)
-        berdly:spin(0)
-        kris.sprite:set("pose")
-        noelle.sprite:set("pose")
-        berdly.sprite:set("pose")
-
-        cutscene:wait(15/30)
-        cutscene:setSpeaker("berdly")
-        cutscene:text("* Onward, [wait:5]my fellow smart students!!", "smug_c")
-
-        cutscene:resetSprites()
-        
-        kris:setFacing("right")
-        noelle:setFacing("right")
-        berdly:setFacing("right")
         cutscene:attachCamera()
         cutscene:interpolateFollowers()
         cutscene:attachFollowers()
