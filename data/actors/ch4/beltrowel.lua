3d20ab04d60bde5ec5a6f30371d527095bcf0316
HUECYCLES <luhlinard@gmail.com>
2025-06-24 19:42:27 -0300

    beltrowel actor + offsets

---
+++ data/actors/ch4/beltrowel.lua
@@ -0,0 +1,76 @@
+local actor, super = Class(Actor, "beltrowel")
+
+function actor:init()
+    super.init(self)
+
+    self.name = "Beltrowel"
+
+    self.width = 25
+    self.height = 41
+
+    self.hitbox = {2, 30, 20, 11}
+
+    self.color = {0.8, 0, 0}
+	
+	self.flip = "right"
+
+    self.path = "npcs/ch4/beltrowel"
+    self.default = "arms_folded"
+
+    self.voice = "beltrowel"
+    self.portrait_path = nil
+    self.portrait_offset = nil
+
+    self.font = "main_mono_alt"
+    self.indent_string = "♪ "
+
+    self.talk_sprites = {}
+
+    self.animations = {}
+    
+    self.offsets = {
+        ["arms_folded"] = {0, 0},
+        ["arms_folded_closed"] = {0, 0},
+
+        ["arms_out"] = {-6, 0},
+        ["arms_out_up"] = {-7, 0},
+
+        ["defeat"] = {-6, 12},
+        ["defeat_look"] = {-6, 10},
+
+        ["limp_wrist"] = {0, 1},
+
+        ["transform_a"] = {-18, -8},
+        ["transform_b"] = {-34, -27},
+    }
+
+    self.angry = false
+    self.voice_timer = 0
+end
+
+function actor:onWorldUpdate(chara)
+    self.voice_timer = Utils.approach(self.voice_timer, 0, DTMULT)
+end
+
+function actor:onTextSound(node, state)
+    if self.voice_timer == 0 then
+        local snd = Assets.stopAndPlaySound("voice/beltrowel")
+        if not self.angry then
+            snd:setPitch(0.8 + Utils.random(0.15))
+        else
+            snd:setPitch(0.9)
+        end
+        self.voice_timer = 1.1
+    end
+    return true
+end
+
+function actor:getIndentString()
+    if self.angry then
+        return "♩ "
+    else
+        return "♪ "
+    end
+end
+
+return actor
