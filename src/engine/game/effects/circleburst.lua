cffab54611be2dae6d8e1fb1108c6c05b2c5ddb2
HUECYCLES <luhlinard@gmail.com>
2025-07-02 16:41:22 -0300

    circleburst update

--- src/engine/game/effects/circleburst.lua
+++ src/engine/game/effects/circleburst.lua
@@ -2,14 +2,18 @@
 ---@overload fun(...) : CircleBurst
 local CircleBurst, super = Class(Object)
 
-function CircleBurst:init(x, y, color, radius, line_width)
+function CircleBurst:init(x, y, color, radius, init_radius, speed, fade_speed, lifetime, thickness)
     super.init(self, x, y)
 
     self.alpha = 1
 
     self.color = color or {1, 1, 1}
-    self.radius = radius or 100
-    self.line_width = line_width or 4
+    self.radius = radius or 80
+    self.init_radius = init_radius or 0
+    self.speed = speed or 2
+    self.fade_speed = fade_speed or 0.05
+    self.lifetime = lifetime or 20
+    self.thickness = thickness or 4
 
     self.layer = BATTLE_LAYERS["above_bullets"]
 
@@ -19,8 +23,10 @@ end
 function CircleBurst:update()
     self.timer = self.timer + DTMULT
 
-    if self.timer >= 20 then
-        self.alpha = self.alpha - 0.1 * DTMULT
+    self.init_radius = Utils.approach(self.init_radius, self.radius, self.speed * DTMULT)
+
+    if self.timer >= self.lifetime and self.init_radius >= self.radius then
+        self.alpha = self.alpha - self.fade_speed * DTMULT
     end
 
     if self.alpha < 0 then
@@ -32,8 +38,8 @@ end
 
 function CircleBurst:draw()
     Draw.setColor(self.color, self.alpha)
-    love.graphics.setLineWidth(self.line_width)
-    love.graphics.circle("line", 0, 0, self.radius + ((self.timer - 20) * 10), 32)
+    love.graphics.setLineWidth(self.thickness)
+    love.graphics.circle("line", 0, 0, self.init_radius, 32)
 
     super.draw(self)
 end
